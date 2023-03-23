84a5c3e037595ad7818db82077c4e8c02282b3c0
Ruslan <rzamorenyj@gmail.com>
2023-03-23 16:16:56 +0200

    do update

--- main_folder/app.py
+++ main_folder/app.py
@@ -95,6 +95,15 @@ def db_connect():
     return connect
 
 
+def finds_id(colum, table, goods_id):
+    c = db_connect().cursor()
+    project_id = f"SELECT {colum} FROM {table} WHERE names_id = %s"
+    argument_for_add = goods_id
+    c.execute(project_id, [argument_for_add])
+    find_id = c.fetchall()
+    return find_id
+
+
 def token_required(f):
     @wraps(f)
     def decorator(*args, **kwargs):
@@ -167,6 +176,7 @@ def logout():
 
 
 @app.route("/add_brand", methods=("POST", "GET"))
+@token_required
 def add_brand():
     brand = request.headers.get("name")
     prise = request.headers.get("cost")
@@ -176,7 +186,6 @@ def add_brand():
 # Create goods
     names = Names(name=brand, description=description, prise=prise)
     db.session.add_all([names])
-    db.session.commit()
 # do relations
     sizes = Sizes.query.get(size_id)
     names.nam.append(sizes)
@@ -188,6 +197,7 @@ def add_brand():
 
 
 @app.route("/add_colour", methods=("POST", "GET"))
+@token_required
 # add colour to table Colours
 def add_colour():
     colour_name = request.headers.get("colour")
@@ -198,6 +208,7 @@ def add_colour():
 
 
 @app.route("/add_size", methods=("POST", "GET"))
+@token_required
 # add size to table Sizes
 def add_size():
     size_number = request.headers.get("size")
@@ -208,6 +219,7 @@ def add_size():
 
 
 @app.route("/del_brand", methods=["DELETE"])
+@token_required
 # del name from table Names
 def del_from_names():
     del_names_id = request.headers.get("del_names_id")
@@ -218,6 +230,7 @@ def del_from_names():
 
 
 @app.route("/del_colour", methods=["DELETE"])
+@token_required
 # del colour from table Colours
 def del_from_colours():
     del_colour_id = request.headers.get("del_colours_id")
@@ -228,6 +241,7 @@ def del_from_colours():
 
 
 @app.route("/del_size", methods=["DELETE"])
+@token_required
 # del size from table Sizes
 def del_from_sizes():
     del_size_id = request.headers.get("del_sizes_id")
@@ -238,6 +252,7 @@ def del_from_sizes():
 
 
 @app.route("/update", methods=["PUT"])
+@token_required
 def update_brands():
     update_name_id = request.headers.get("update_name_id")
     if not update_name_id:
@@ -252,21 +267,22 @@ def update_brands():
     user_size = Sizes.query.get(update_size_choose_shoes_id)
     user_colour = Colours.query.get(update_colour_choose_shoes_2_id)
     user = Names.query.get(update_name_shoes_id)
-    if update_name:
-        user.name = update_name
-    if update_description:
-        user.description = update_description
-    if update_prise:
-        user.prise = update_prise
-    if user_size:
-        user.nam = [user_size]
-    if user_colour:
-        user.nam_2 = [user_colour]
+    list_for_checking = [update_name, update_description, update_prise, user_size, user_colour]
+    for item in list_for_checking:
+        if item:
+            user.name = update_name
+            user.description = update_description
+            user.prise = update_prise
+            user.nam = [user_size]
+            user.nam_2 = [user_colour]
+        else:
+            return 'Please check your data again'
     db.session.commit()
     return "success"
 
 
 @app.route("/info_one", methods=["GET"])
+@token_required
 def info_one():
     get_name_id = request.headers.get("get_name_id")
     get_size_id = request.headers.get("get_size_id")
@@ -279,13 +295,34 @@ def info_one():
                  "user_name": user.name,
                  "user_description": user.description,
                  "user_prise": user.prise,
+                 "user_size_id": user_size.id,
                  "user_size": user_size.size,
+                 "user_colour_id": user_colour.id,
                  "user_colour": user_colour.colour}
     return jsonify(str(user_dict))
 
 
 @app.route("/info_all", methods=["GET"])
+@token_required
 def info_many():
+    def filter_goods(user_list, filter_key, filter_value):
+        try:
+            values = [int(filter_value)]
+            filter_list = [d for d in user_list if d[filter_key] in values]
+            return filter_list
+        except:
+            values = [filter_value]
+            filter_list = [d for d in user_list if d[filter_key] in values]
+            return filter_list
+
+    def sort_goods(user_list, sort_key, sort_value, less_or_more):
+        if less_or_more == "less":
+            sort_list = [d for d in user_list if d[sort_key] <= int(sort_value)]
+            return sort_list
+        elif less_or_more == "more":
+            sort_list = [d for d in user_list if d[sort_key] > int(sort_value)]
+            return sort_list
+
     filter_value = request.headers.get("filter_value")
     filter_key = request.headers.get("filter_key")
     sort_key = request.headers.get("sort_key")
@@ -299,16 +336,12 @@ def info_many():
 
     for user_id in users:
         user = Names.query.get(user_id.id)
-        colour_id = "SELECT colours_id FROM choose_shoes_2 WHERE names_id = %s"
-        argument_1_for_get = user_id.id
-        c.execute(colour_id, [str(argument_1_for_get)])
-        find_id = c.fetchall()
-        size_id = "SELECT sizes_id FROM choose_shoes WHERE names_id = %s"
-        argument_2_for_get = user_id.id
-        c.execute(size_id, [argument_2_for_get])
-        find_size_id = c.fetchall()
-        if find_id and find_size_id:
-            user_colour = Colours.query.get(find_id)
+
+        find_colour_id = finds_id('colours_id', 'choose_shoes_2', user_id.id)
+        find_size_id = finds_id('size_id', 'choose_shoes', user_id.id)
+
+        if find_colour_id and find_size_id:
+            user_colour = Colours.query.get(find_colour_id)
             user_size = Sizes.query.get(find_size_id)
         else:
             user_colour = None
@@ -322,24 +355,12 @@ def info_many():
                      "user_colour_id": user_colour.id if user_colour else None,
                      "user_colour": str(user_colour)}
         user_list.append(user_dict)
-# Filter goods
     if filter_value and filter_key:
-        try:
-            values = [int(filter_value)]
-            filter_list = [d for d in user_list if d[filter_key] in values]
-            return jsonify(str(filter_list))
-        except:
-            values = [filter_value]
-            filter_list = [d for d in user_list if d[filter_key] in values]
-            return jsonify(str(filter_list))
-# Sort goods
+        filter_goods(user_list, filter_key, filter_value)
+        return jsonify(str(filter_goods))
     elif sort_key and sort_value:
-        if less_or_more == "less":
-            sort_list = [d for d in user_list if d[sort_key] <= int(sort_value)]
-            return jsonify(str(sort_list))
-        elif less_or_more == "more":
-            sort_list = [d for d in user_list if d[sort_key] > int(sort_value)]
-            return jsonify(str(sort_list))
+        sort_goods(user_list, sort_key, sort_value, less_or_more)
+        return jsonify(str(sort_goods))
     else:
         return jsonify(str(user_list))
 
@@ -355,16 +376,10 @@ def add_basket(current_user):
 
     customer = current_user.id
     shoes = Names.query.get(id_shoes)
+    find_colour_id = finds_id('colours_id', 'choose_shoes_2', shoes.id)
+    find_size_id = finds_id('size_id', 'choose_shoes', shoes.id)
 
-    colour_id = "SELECT colours_id FROM choose_shoes_2 WHERE names_id = %s"
-    argument_colour_add = shoes.id
-    c.execute(colour_id, [str(argument_colour_add)])
-    find_id = c.fetchall()
-    size_id = "SELECT sizes_id FROM choose_shoes WHERE names_id = %s"
-    argument_size_add = shoes.id
-    c.execute(size_id, [argument_size_add])
-    find_size_id = c.fetchall()
-    user_colour = Colours.query.get(find_id)
+    user_colour = Colours.query.get(find_colour_id)
     user_size = Sizes.query.get(find_size_id)
     basket = Basket(id_record=num_record, id_good=shoes.id, id_colour_shoes=user_colour.id, id_size_shoes=user_size.id,
                     status=stat, all_summ=shoes.prise, id_customer=customer)
@@ -374,17 +389,16 @@ def add_basket(current_user):
 
 
 @app.route('/change_status')
+@token_required
 def status_change():
     new_status = request.headers.get("new_status")
     id_for_change = request.headers.get("id_for_change")
-    basket_status = Basket.query.get(id_for_change)
+    basket_id_for_change = Basket.query.get(id_for_change)
     if new_status:
-        basket_status = Basket.query.get(id_for_change)
-        basket_status.status = new_status
+        basket_id_for_change.status = new_status
     else:
-        if basket_status.status == "in the basket":
-            del_obj = Basket.query.get(id_for_change)
-            db.session.delete(del_obj)
+        if basket_id_for_change.status == "in the basket":
+            db.session.delete(basket_id_for_change)
             return "success delete "
         else:
             return "Your status don`t in the basket"
